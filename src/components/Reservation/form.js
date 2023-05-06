ee3bec1d03bbf8af15e3ebcf5fe930b3a38a772c
pucha12 <ls52336@ubt-uni.net>
2023-05-07 00:41:45 +0200

    style fixes on form component, adding a search button

--- src/components/Reservation/form.js
+++ src/components/Reservation/form.js
@@ -1,23 +1,50 @@
 import React from "react";
 import classes from './form.module.css'
-const Form = (props) =>{
+const Form = (props) => {
 
-    return (
-      <div className={classes.formdiv}>
-        <form className={classes.form}>
-          <label>Name:</label>
-          <input className={classes.input} type="text" name="name" />
-          <label>Name:</label>
-          <br></br>
-          <input className={classes.input} type="text" name="name" />
-          <label>Name:</label>
-           <input className={classes.inputdate} type="date" name="name" />
-          <label>Name:</label>
-          <br></br>
-          <input className={classes.inputdate} type="date" name="name" />
-        </form>
-      </div>
-    );
+  return (
+    <div className={classes.formdiv}>
+      <form className={classes.form}>
+      <div className={classes.checkboxDiv}>
+          <input className={classes.checkboxInput} type="radio" name="roundTip"/>
+          <label className={classes.checkboxLabel}>Round Trip</label>
+
+          <input className={classes.checkboxInput} type="radio" name="oneWay"/>
+          <label className={classes.checkboxLabel}>One way</label>
+
+          <input className={classes.checkboxInput} type="radio" name="multiCity"/>
+          <label className={classes.checkboxLabel}>Multi City</label>
+        </div>
+        <div className={classes.inputDiv}>
+          <label className={classes.formLabel}>From</label>
+          <select className={classes.input} name="from">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </select>
+        </div>
+        <div className={classes.inputDiv}>
+          <label className={classes.formLabel}>To</label>
+          <select className={classes.input} name="to">
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </select>
+        </div>
+        <div className={classes.inputDateDiv}>
+          <label className={classes.formLabel}>Departing</label>
+          <input className={classes.inputdate} type="date" name="departing" />
+        </div>
+        <div className={classes.inputDateDiv}>
+          <label className={classes.formLabel}>Returning</label>
+          <input className={classes.inputdate} type="date" name="returning" />
+        </div>
+        <div className={classes.buttonDiv}>
+          <button className={classes.button}>Search For Tickets</button>
+        </div>
+      </form>
+    </div>
+  );
 
 };
 export default Form
