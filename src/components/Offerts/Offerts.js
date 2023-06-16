488a7fe6ecd860cfafbd4e16a4b2ef455e1dd52c
pucha12 <ls52336@ubt-uni.net>
2023-06-16 21:25:39 +0200

    connecting offers with database

--- src/components/Offerts/Offerts.js
+++ src/components/Offerts/Offerts.js
@@ -1,15 +1,41 @@
- import React from "react"
- import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
- import Header from "../Header";
- const Offers = (props) =>{
+import React from "react"
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Header from "../Header";
+import { useEffect, useState } from "react";
+import axios from "axios";
+const Offers = (props) => {
 
+    const [offers, setOffers] = useState([]);
 
+    useEffect(() => {
+        async function fetchData() {
+            const data = (await axios.get("https://localhost:5001/api/Offer")).data;
+            setOffers(data);
+        }
+        fetchData();
+    }, []);
     return (
         <div>
-        <Header/>
-        <h1>THANKS FOR THIS PEDROTECH</h1>
+            <Header />
+            {offers.map((offer) => (
+                <div key={offer.OfferId}>
+                    <div>
+                        <h1>Image</h1>
+                    </div>
+                    <div>
+                        <h1>{offer.OfferName}</h1>
+                        <h1>{offer.description}</h1>
+                    </div>
+                    <div>
+                        <h1>{offer.price}</h1>
+                    </div>
+                    <div>
+                        <h1>rezervo</h1>
+                    </div>
+                </div>
+            ))}
         </div>
     )
- }
+}
 
- export default Offers
+export default Offers
