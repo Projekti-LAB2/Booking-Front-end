f8f2b79fb959619e40350e6f5fdeafbcf977d312
DrenVisoka <dv51406@ubt-uni.net>
2023-06-21 01:59:50 +0200

    fixing login signup buttons

--- src/components/HeaderButton.js
+++ src/components/HeaderButton.js
@@ -4,6 +4,23 @@ import Langselect from "./langselect/Langselect";
 
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 
+const redirectToLogin = () => {
+  const currentDomain = window.location.href;
+
+  if (currentDomain.includes("http://localhost:3000/")) {
+    let newDomain = currentDomain.replace("http://localhost:3000/", "http://localhost:4200/login");
+    window.location.replace(newDomain);
+  }
+};
+const redirectToSignup = () => {
+  const currentDomain = window.location.href;
+
+  if (currentDomain.includes("http://localhost:3000/")) {
+    let newDomain = currentDomain.replace("http://localhost:3000/", "http://localhost:4200/signup");
+    window.location.replace(newDomain);
+  }
+};
+
 const HeaderButton = () => {
   return (
     <div className='listHeader'>
@@ -17,8 +34,8 @@ const HeaderButton = () => {
         <Langselect/>
       </ul>
       <div>
-        <button className="buttonHeader">Login</button>
-        <button className="buttonHeader">Sign up</button>
+        <button className="buttonHeader" onClick={redirectToLogin}>Login</button>
+        <button className="buttonHeader" onClick={redirectToSignup}>Sign up</button>
       </div>
     </div>
   );
